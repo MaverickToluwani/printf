51e505a2907fb286a90c21ae7154eb8052b834ed
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-02-02 12:18:50 -0800

    _printf.c indent correction

--- _printf.c
+++ _printf.c
@@ -1,27 +1,30 @@
 #include "main.h"
-/
-int _print(const char* format, ...)
+/**
+ * _printf - Displays inputs
+ * @format: String argument passed to _print
+ * Return: count
+ */
+int _print(const char *format, ...)
 {
 	int count;
-	
+
 	if (format == NULL)
 		return (-1);
-	
 	va_list arg;
+
 	va_start(arg, format);
 	count = 0;
 	while (*format != '\0')
 	{
-		if(*format == '%')
-        {
-		++format;
-		count += format_print(*format, arg);
-        }
-        else
-		count += write(1, format, 1);
-        format++;
-    }
-
-    va_end(arg);
-    return count;
+		if (*format == '%')
+		{
+			++format;
+			count += format_print(*format, arg);
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(arg);
+	return (count);
 }
